28122dacfd49fe81b601cb57ec456f57ab0b6e3d
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-03 11:03:57 +0300

    ft: added app views configurations

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import (ListView)
+
+
 
 # Create your views here.
+
+class PostListView(ListView):
+    template_name = "insta/post_list.html"
+    querryset = Post.objects.all()
+    context_object_name = 'posts'
+    
