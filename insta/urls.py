5a08a4dc7a7ead993f278da88e4def25d2f9ce88
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-04 09:21:34 +0300

    ft:added post create template

--- insta/urls.py
+++ insta/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import (PostListView)
+from .views import (PostListView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView)
 
 
 app_name = 'insta'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
-]
+    path('new/', PostCreateView.as_view(), name='post_create'), 
+]  
