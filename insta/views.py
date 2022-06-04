f8c08bb31cd655d3c705e3369f7db0d7f17de48d
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-04 09:09:28 +0300

    test: added views logic

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import (ListView)
+from django.views.generic import (ListView, CreateView, UpdateView, DeleteView)
 
 
 
@@ -11,3 +11,8 @@ class PostListView(ListView):
     queryset = Post.objects.all()
     context_object_name = 'posts'
     
+    
+    
+class PostCreateView(CreateView):
+    template = 'insta/post_create.html'
+    from_class = PostForm
